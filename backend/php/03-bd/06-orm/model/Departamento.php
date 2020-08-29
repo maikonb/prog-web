687f10234adc47f89a41af58f869e69e98157aba
maikon <maikon@gmail.com>
2020-08-29 05:15:17 -0400

    add models

--- backend/php/03-bd/06-orm/model/Departamento.php
+++ backend/php/03-bd/06-orm/model/Departamento.php
@@ -1,1 +1,29 @@
 <?php 
+
+class Departamento {
+
+  private $id;
+  private $nome;
+
+  public function __construct($id, $nome) {
+      $this->id = $id;
+      $this->nome = $nome;
+  }
+
+  public function setId($id) {
+      $this->id = $id;
+  }
+
+  public function getId() {
+      return $this->id;
+  }
+
+  public function setNome($nome) {
+      $this->nome = $nome;
+  }
+
+  public function getNome() {
+      return $this->nome;
+  }
+  
+};
--- backend/php/03-bd/06-orm/model/Produto.php
+++ backend/php/03-bd/06-orm/model/Produto.php
@@ -1,1 +1,72 @@
 <?php 
+
+require_once('../model/marca.php');
+require_once('../model/Marca.php');
+
+class Produto {
+    
+  private $id;
+  private $nome;
+  private $preco;
+  private $estoque;
+  private $marca;
+  private $departamentos = [];
+
+  public function __construct(int $id=-1, string $nome="", float $preco=0.0, Marca $marca=null) {
+      $this->id = $id;
+      $this->nome = $nome;
+      $this->preco = $preco;
+      $this->marca = $marca;
+      $this->departamentos = [];
+  }
+  
+  public function setId($id) {
+      $this->id = $id;
+  }
+
+  public function getId() {
+      return $this->id;
+  }
+  
+  public function setNome(string $nome) {
+      $this->nome = $nome;
+  }
+
+  public function getNome() {
+      return $this->nome;
+  }
+  
+  public function setPreco(float $preco) {
+      $this->preco = $preco;
+  }
+
+  public function getPreco() {
+      return $this->preco;
+  }
+
+  public function setMarca(Marca $marca) {
+      $this->marca = $marca;
+  }
+  
+  public function getMarca() {
+      return $this->marca;
+  }
+
+  public function setEstoque(int $estoque) {
+    $this->estoque = $estoque;
+  }
+
+  public function getEstoque() {
+    return $this->estoque;
+  }
+
+  public function setDepartamentos(array $departamentos) {
+    $this->departamentos = $departamentos;
+  }
+
+  public function getDepartamentos() {
+    return $this->departamentos;
+  }
+};
+
+
