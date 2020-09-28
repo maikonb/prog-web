df0dc53d7bcff67026a4aecb6c5af92c2aae6fb8
maikon <maikon@gmail.com>
2020-09-28 16:01:41 -0400

    Departamentos OK

---
+++ frameworks/laravel/04-crud-completo/resources/views/departamentos/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.principal')
+
+@section('main')
+
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Cadastro de Departamentos</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-12" >
+
+            <form action="{{ route('departamentos.store') }}" class="card p-2 my-4" method="POST">
+                @csrf
+                <div class="input-group">
+                    <input type="text" placeholder="Nome da Departamento" 
+                        class="form-control" name="nome" required>
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-primary">
+                            Salvar
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <a href="{{ route('departamentos.index') }}" 
+            class="btn btn-secondary ml-1" role="button" aria-pressed="true">Cancelar</a>
+
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ frameworks/laravel/04-crud-completo/resources/views/departamentos/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.principal')
+
+@section('main')
+
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Departamento</h2>
+    </div>
+    <div class="row">
+        <h3>
+          ID: {{ $departamento->id }}
+        </h3>
+    </div>
+    <div class="row">       
+        <h3>
+          Nome: {{ $departamento->nome }}
+        </h3>          
+    </div>
+    
+    <div class="row mt-5">
+      
+      <h4>Produtos:</h4>
+
+      <div class="col-md-12" >
+        
+        @if(count($produtos) == 0)
+          <p> Nenhum produto associado.</p>
+        @endif
+
+      </div>
+    </div>        
+
+    <div class="row mt-5">
+      <a href="{{ route('departamentos.index') }}" 
+        class="btn btn-primary" role="button" aria-pressed="true">Voltar</a>
+    </div>
+  </div>
+
+</div>
+
+@endsection
